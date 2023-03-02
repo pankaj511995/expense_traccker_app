6d8e69b90d466639e5d5fd83f4467467f8a145f6
pankaj511995 <kpankaj511995@gmail.com>
2023-03-02 12:34:19 +0530

    signin_html

--- controller/userdetails.js
+++ controller/userdetails.js
@@ -4,7 +4,14 @@ exports.signupUser=(req,res)=>{
    User.create({name:name,email:email,password:password}).then(e=>res.status(200).json({status:true}))
    .catch(e=>res.status(400).json('email already exist'))
 }
-
-// module.exports={
-//     signpuUser
-// }
+exports.signinUser=(req,res)=>{
+    const {email,password}=req.body
+    User.findAll({where:{email:email}}).then(e=>{
+        if(e[0].password===Number(password)){
+            res.status(200).json({success:true})
+        }else{
+            res.status(404).json({messaga:'incorrect password'})
+        }
+        
+    }).catch(e=>res.status(404).json({messaga:'new user signup now'}))
+}
